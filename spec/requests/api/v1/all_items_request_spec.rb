bb1bab6b1b80953c4565d4b0e8aad76a0638d1e7
JunePaloma <kborkoski@gmail.com>
2017-11-01 11:51:50 -0600

    item not creating in test for initial API provide of all items

---
+++ spec/requests/api/v1/all_items_request_spec.rb
@@ -0,0 +1,12 @@
+describe "Items API" do
+  it "sends a list of items" do
+    Item.create(name: "Item1", description: "I'm an item", image_url: "some_url")
+    Item.create(name: "Item2", description: "I'm also an item", image_url: "some_url")
+
+    get "/api/v1/items"
+    items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(items.count).to eq(2)
+  end
+end
