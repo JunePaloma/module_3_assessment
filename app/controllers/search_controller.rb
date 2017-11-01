1c68c2745775e9e75e0aafc23bdeab33bd3f0fbc
JunePaloma <kborkoski@gmail.com>
2017-11-01 10:20:14 -0600

    update request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-  @conn = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV[API_KEY]}") do |faraday|
+  @conn = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=toreId,storeType,longName,phone,distance,city&pageSize=10&apiKey=ENV[API_KEY]") do |faraday|
     faraday.adapter Faraday.default_adapter
     end
   end
