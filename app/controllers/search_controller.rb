e333c9a1e2c4bea5973bdacc6c204f65cbf45eab
JunePaloma <kborkoski@gmail.com>
2017-11-01 10:32:09 -0600

    Still fighting with the API

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,14 @@
 class SearchController < ApplicationController
   def index
-  @conn = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=toreId,storeType,longName,phone,distance,city&pageSize=10&apiKey=ENV[API_KEY]") do |faraday|
+  @conn = Faraday.new(url: "https://api.bestbuy.com/v1/storesarea") do |faraday|
     faraday.adapter Faraday.default_adapter
     end
+
+
+  #bring in params for zipcode
+  response = @conn.get("area(80202,25))?format=json&show=storeId,storeType,longName,phone,distance,city&pageSize=10&apiKey=#{ENV["API_KEY"]}")
+binding.pry
+    @stores = JSON.parse(response.body, symbolize_names: true)[:stores]
+
   end
 end
